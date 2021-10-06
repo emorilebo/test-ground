6f670ebe35c61be1b1f7c20b70222fddb05285f9
emorilebo <emorylebo@gmail.com>
2021-10-06 02:38:31 +0100

    configurations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userName: "Emori",
+      todoItems: [
+        {action: "Buy Milk", done: false},
+        {action: 'Dentist at 5pm', done: false},
+        {action: 'Go to the Gym', done: false},
+      ]
+    };
+  }
+
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Emori" ? "Godfrey" : "Emori",
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <div className="flex-row items-center justify-center">
+            <h2 className="flex-grow-0  bg-blue-800 text-white text-2xl">
+              {this.state.userName} Todo List
+            </h2>
+            <button
+              className="text-red-900 border-2 p-2 rounded bg-gray-200"
+              onClick={this.changeStateData}
+            >
+              Change
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
